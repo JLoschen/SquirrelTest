914517e39d4bfe8abad54f4398f6b95963b9360b
Joshua Loschen <josh.loschen@gmail.com>
2019-07-29 20:42:27 -0500

    Test application for Squirrel .NET installer.

---
+++ SquirrelTest/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Squirrel;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+namespace SquirrelTest
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Update();
+        }
+
+        private async Task Update()
+        {
+            using(var manager = new UpdateManager(@"C: \Users\Josh\Desktop\Releases"))
+            {
+                await manager.UpdateApp();
+            }
+        }
+    }
+}
